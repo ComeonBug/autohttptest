e4c5a793b4b7b95c31999a27041fe53c351a10c1
bluebluepupu <309351223@qq.com>
2024-04-18 14:57:31 +0800

    add new data yaml

--- testcases/test_baidu.py
+++ testcases/test_baidu.py
@@ -1,10 +1,14 @@
 import allure
 import pytest
+import pytest_ordering
 
 class TestBaidu:
 
     # @pytest.mark.parametrize('',[])
+    id = [1,2]
+
     @pytest.mark.P0
+    @pytest.mark.run(order=id[1])
     def test_baidu_P0(self):
         casename = 'casename'
         allure.dynamic.title(casename)
@@ -14,12 +18,23 @@ class TestBaidu:
     # @pytest.mark.parametrize('',[])
     @pytest.mark.P0
     @pytest.mark.P1
+    @pytest.mark.run(order=id[0])
+    @pytest.mark.dependency()
     def test_baidu_P1(self):
         casename = 'casename'
         allure.dynamic.title(casename)
         print('test_baidu_P0_P1')
         assert 1 ==1
 
+    # @pytest.mark.parametrize('',[])
+    @pytest.mark.P0
+    @pytest.mark.P1
+    @pytest.mark.dependency()
+    def test_baidu_dependency(self):
+        casename = 'casename'
+        allure.dynamic.title(casename)
+        print('test_baidu_P0_P1')
+        assert 1 ==1
 
 if __name__ == '__main__':
     pytest.main(['-vss','-m P0'])
